943018736938ea62f7ba72476be8df17b46bcd86
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-05-06 17:48:10 +0530

    Updated the get all links logic

--- src/utils/generateStaticPaths.ts
+++ src/utils/generateStaticPaths.ts
@@ -4,33 +4,33 @@ import { languages } from './langs'
 
 export default async function generateStaticPaths() {
   const storyblokApi = useStoryblokApi()
-  const { data } = await storyblokApi.get('cdn/links', {
+  const links = await storyblokApi.getAll('cdn/links', {
     version: isPreview() ? 'draft' : 'published',
   })
-  let links = data.links
-  links = Object.values(links)
   let paths = []
-  links.forEach((link: { slug: string }) => {
-    languages.forEach((language) => {
-      //This slug will be used for fetching data from storyblok
-      let slug = link.slug === 'home' ? undefined : link.slug
-      //This will be used for generating all the urls for astro
-      let full_url = language === 'en' ? slug : `${language}/${slug ?? ''}`
-      //This will let us change the url for diffrent versions
-      let langSwitch = {}
-      languages.forEach((lang) => {
-        langSwitch = {
-          ...langSwitch,
-          [lang]: lang === 'en' ? `/${slug ?? ''}` : `/${lang}/${slug ?? ''}`,
-        }
-      })
-      paths.push({
-        props: { language, slug, langSwitch },
-        params: {
-          slug: full_url,
-        },
+  links
+    .filter((link) => !link.is_folder)
+    .forEach((link: { slug: string }) => {
+      languages.forEach((language) => {
+        //This slug will be used for fetching data from storyblok
+        let slug = link.slug === 'home' ? undefined : link.slug
+        //This will be used for generating all the urls for astro
+        let full_url = language === 'en' ? slug : `${language}/${slug ?? ''}`
+        //This will let us change the url for diffrent versions
+        let langSwitch = {}
+        languages.forEach((lang) => {
+          langSwitch = {
+            ...langSwitch,
+            [lang]: lang === 'en' ? `/${slug ?? ''}` : `/${lang}/${slug ?? ''}`,
+          }
+        })
+        paths.push({
+          props: { language, slug, langSwitch },
+          params: {
+            slug: full_url,
+          },
+        })
       })
     })
-  })
   return paths
 }
